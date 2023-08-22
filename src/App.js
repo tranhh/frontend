14fd481ace701a9fd1a474b27f8993d1a9762ceb
tranhh <tranhnguyen8a1@gmail.com>
2023-08-22 14:47:03 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import './App.css';
+import HomePage from './screens/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element ={<HomePage/>} exact></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
